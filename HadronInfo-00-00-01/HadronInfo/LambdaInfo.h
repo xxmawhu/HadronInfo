1a2487bd92ccad96e92a8f9e391cfbd37564bbcb
xxmawhu <xxmawhu@163.com>
2020-01-22 14:09:40 +0800

    update

--- HadronInfo-00-00-01/HadronInfo/LambdaInfo.h
+++ HadronInfo-00-00-01/HadronInfo/LambdaInfo.h
@@ -3,17 +3,17 @@
 #
 #   Author        : Xin-Xin MA
 #   Email         : xxmawhu@163.com
-#   File Name     : NewKsInfo.h
+#   File Name     : LambdaInfo.h
 #   Create Time   : 2019-12-12 15:31
 #   Last Modified : 2020-01-15 22:29
 #   Describe      :
 #
 # ====================================================*/
-#ifndef HadronInfo_NewKsInfo_H
-#define HadronInfo_NewKsInfo_H
+#ifndef HadronInfo_LambdaInfo_H
+#define HadronInfo_LambdaInfo_H
 #include "HadronInfo/CombInfo.h"
 #include "HadronInfo/ProtonInfo.h"
 #include "HadronInfo/PionInfo.h"
-typedef CombInfo<PionInfo, PionInfo, 310, 1> NewKsInfo;
+typedef CombInfo<ProtonInfo, PionInfo, 3122, 1> LambdaInfo;
 
 #endif
